f8ccda5e457b8358e7749fe90e217f2ebd5b7720
vgneushev <vgneushev@it-one.ru>
2025-03-10 17:19:23 +0300

    [+] thymeleaf template

--- src/main/java/com/mvcdemo/app/estore/controller/HomeController.java
+++ src/main/java/com/mvcdemo/app/estore/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.mvcdemo.app.estore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String homePage() {
-        return "home";
+    public ModelAndView homePage() {
+
+        Map<String, String> model = new HashMap<>();
+        model.put("firstName", "Vlad");
+
+        ModelAndView modelAndView = new ModelAndView("home", model);
+        return modelAndView;
     }
 
 
