021060980303895281e9d9c4745231df4c7ad50e
vgneushev <vgneushev@it-one.ru>
2025-03-10 17:28:08 +0300

    [+] URI path params

---
+++ src/main/java/com/mvcdemo/app/estore/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.mvcdemo.app.estore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping(path = "/users/{userId}/albums/{albumId}")
+    public ModelAndView getAlbum(@PathVariable("userId") String userId,
+                                @PathVariable("albumId") String albumId) {
+
+        ModelAndView modelAndView = new ModelAndView("album");
+        modelAndView.addObject("userId", userId);
+        modelAndView.addObject("albumId", albumId);
+
+
+        return modelAndView;
+    }
+}
