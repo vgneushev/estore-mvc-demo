586776b22cfa26f67e0eecd6de4460fc16446209
vgneushev <vgneushev@it-one.ru>
2025-03-10 17:42:35 +0300

    [+] read query params

--- src/main/java/com/mvcdemo/app/estore/controller/UserController.java
+++ src/main/java/com/mvcdemo/app/estore/controller/UserController.java
@@ -3,6 +3,7 @@ package com.mvcdemo.app.estore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,7 +17,13 @@ public class UserController {
         modelAndView.addObject("userId", userId);
         modelAndView.addObject("albumId", albumId);
 
+        return modelAndView;
+    }
 
+    @GetMapping(path = "/users")
+    public ModelAndView getUsers(@RequestParam(name = "limit", defaultValue = "30") int limit) {
+
+        ModelAndView modelAndView = new ModelAndView("users");
         return modelAndView;
     }
 }
